7c783ba430ee709be7e6eabb5e6e638771d51b96
PacolasPL <82538410+PacolasPL@users.noreply.github.com>
2023-02-11 19:14:43 -0500

    Rebase Based on Nicolas skeleton
    Esqueleto

--- eggcrack/src/main/java/edu/uniandes/Main.java
+++ eggcrack/src/main/java/edu/uniandes/Main.java
@@ -1,13 +1,15 @@
 package edu.uniandes;
 
 import edu.uniandes.factory.Product;
+import edu.uniandes.factory.ProductFactoryBuilder;
 import edu.uniandes.factory.worker.RedWorker;
 import edu.uniandes.storage.FiniteMailbox;
 
 public class Main {
+
   public static void main(String[] args) {
+    // Test red worker
     var productSize = 5;
-    var lock = new Object();
 
     var mailbox = new FiniteMailbox<>(5, Product.class);
     mailbox.send(new Product(4, "four"));
@@ -17,7 +19,15 @@ public class Main {
     mailbox.send(new Product(0, "zero"));
     // mailbox.displayMailbox();
 
-    var redWorker = new RedWorker(productSize, mailbox, lock);
+    var redWorker = new RedWorker(productSize, mailbox);
     redWorker.run();
+
+    // Main
+    /*
+    var pfb = new ProductFactoryBuilder();
+    var pf = pfb.build();
+
+    pf.run();
+     */
   }
 }
